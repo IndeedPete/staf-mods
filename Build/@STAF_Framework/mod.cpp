e1bbeb9955355618fda02fa879b8e637652a73f6
IndeedPete <pete.indeed@gmx.de>
2017-01-12 19:11:52 +0100

    Added built files for STAF_FRAMEWORK.

---
+++ Build/@STAF_Framework/mod.cpp
@@ -0,0 +1,7 @@
+name = "STAF Framework";
+picture = "STAF_Logo.paa";
+actionName = "Website";
+action ="http://stafclan.com/index.php/BoardList/";
+overview = "STAF Framework - Functions and shit for people and shit.";
+logo = "STAF_Logo.paa";
+logoOver = "STAF_Logo_Desat.paa";
