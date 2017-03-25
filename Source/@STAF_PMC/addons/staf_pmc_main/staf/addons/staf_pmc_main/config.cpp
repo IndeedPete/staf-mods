b188fce1dd714b289d305c84561c90470fd0a781
Moony <wildsevenmusic@googlemail.com>
2017-03-25 01:35:01 +0100

    Changed Structure (?)
    Why did that not get committed!

--- Source/@STAF_PMC/addons/staf_pmc_main/staf/addons/staf_pmc_main/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "CfgUnitInsignia.hpp"
-#include "CfgPatches.hpp"
-#include "CfgFaces.hpp"
-#include "CfgFactionClasses.hpp"
-#include "CfgGroups.hpp"
-#include "CfgWorlds.hpp"
-#include "CfgEditorSubcategories.hpp"
