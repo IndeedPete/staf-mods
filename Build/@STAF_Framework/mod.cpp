5190964c885391e6922ab4738124317ee2a7bee4
Moony <wildsevenmusic@googlemail.com>
2017-03-25 02:03:07 +0100

    Deleted Builds
    Because the easy way doesn't work!

--- Build/@STAF_Framework/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "STAF Framework";
-picture = "STAF_Logo.paa";
-actionName = "Website";
-action ="http://stafclan.com/index.php/BoardList/";
-overview = "STAF Framework - Functions and shit for people and shit.";
-logo = "STAF_Logo.paa";
-logoOver = "STAF_Logo_Desat.paa";
