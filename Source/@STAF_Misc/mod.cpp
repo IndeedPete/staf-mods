1de2fef2005badc730bdbe56ef55a1f805305be2
Moony <moony@stafclan.com>
2018-02-01 19:24:46 +0100

    Moved STAF_framework to STAF_Misc
    Just so that it goes together with our Repo layout!

---
+++ Source/@STAF_Misc/mod.cpp
@@ -0,0 +1,7 @@
+name = "STAF MISC";
+picture = "STAF_Logo.paa";
+actionName = "Website";
+action ="http://stafclan.com/index.php/BoardList/";
+overview = "STAF MISC - All the random stuff!";
+logo = "STAF_Logo.paa";
+logoOver = "STAF_Logo_Desat.paa";
