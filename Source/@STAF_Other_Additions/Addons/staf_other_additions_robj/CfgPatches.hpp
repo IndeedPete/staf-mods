f18eaa7bd95a4b9f6e8a51ec266e6b5f9d2f6f4c
Moony <wildsevenmusic@googlemail.com>
2019-04-21 13:37:58 +0200

    Added RobJ's Mission Assets to Vanilla Factions!

---
+++ Source/@STAF_Other_Additions/Addons/staf_other_additions_robj/CfgPatches.hpp
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////////
+
+class CfgPatches {
+    class STAF_other_additions_robj {
+      units[] = {
+        "STAF_RobJ_Rescue1",
+        "STAF_ROBJ_IDAP_HURON_1",
+        "STAF_NAVY_GHOSTHAWK_1",
+        "STAF_ROBJ_IDAP_HELLCAT_1",
+        "STAF_ROBJ_IDAP_ORCA_1",
+        "STAF_ROBJ_IDAP_TARU_1",
+        "STAF_RobJ_Rescue_Van1",
+        "STAF_RobJ_Police_Offroad_01",
+        "STAF_RobJ_Rescue_F",
+        "STAF_RobJ_Police_Officer",
+        "STAF_RobJ_IDAP_Doctor"
+      };
+      weapons[] = {};
+      requiredAddons[] = {"ROBJ_RESCUE_PACK","A3_Air_F_Beta_Heli_Transport_01"};
+      requiredVersion = 1.62;
+      author = "STAF";
+      url = "http://www.stafclan.com";
+    };
+};
---
+++ Source/@STAF_Other_Additions/mod.cpp
@@ -0,0 +1,7 @@
+name = "STAF Other Additions";
+picture = "STAF_Logo.paa";
+actionName = "Website";
+action ="http://stafclan.com/index.php/BoardList/";
+overview = "Some smaller additions to Other Factions";
+logo = "STAF_Logo.paa";
+logoOver = "STAF_Logo_Desat.paa";
