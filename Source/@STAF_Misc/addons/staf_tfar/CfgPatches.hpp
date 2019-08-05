0dc0b8cf791382218aaa3c0d79ce9bfc4fb9bc3c
Moony <wildsevenmusic@googlemail.com>
2019-08-05 18:11:36 +0200

    Added TFAR compatibility to Contact Radio Backpacks

---
+++ Source/@STAF_Misc/addons/staf_tfar/CfgPatches.hpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+	class STAF_TFAR
+	{
+		units[] =
+		{
+			"STAF_B_RadioBag_01_black_F",
+			"STAF_B_RadioBag_01_digi_F",
+			"STAF_B_RadioBag_01_eaf_F",
+			"STAF_B_RadioBag_01_ghex_F",
+			"STAF_B_RadioBag_01_hex_F",
+			"STAF_B_RadioBag_01_mtp_F",
+			"STAF_B_RadioBag_01_tropic_F",
+			"STAF_B_RadioBag_01_oucamo_F",
+			"STAF_B_RadioBag_01_wdl_F"
+		};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {
+			"A3_Supplies_F_Enoch_Bags",
+		};
+		author = "STAF";
+		authorUrl = "http://stafclan.com/index.php/BoardList/";
+		version = 1.0;
+		versionStr = "1.0";
+		versionAr[] = {1,0};
+	};
+};
---
+++ Source/@STAF_Misc/addons/staf_tfar/CfgVehicles.hpp
@@ -0,0 +1,159 @@
+class CfgVehicles
+{
+	class B_RadioBag_01_base_F;
+
+	class B_RadioBag_01_black_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_digi_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_eaf_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_ghex_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_hex_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_mtp_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_tropic_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_oucamo_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+	class B_RadioBag_01_wdl_F: B_RadioBag_01_base_F
+	{
+    scope = 1;
+    scopeCurator = 1;
+	};
+
+	class STAF_B_RadioBag_01_black_F: B_RadioBag_01_black_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_digi_F: B_RadioBag_01_digi_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_eaf_F: B_RadioBag_01_eaf_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_ghex_F: B_RadioBag_01_ghex_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_hex_F: B_RadioBag_01_hex_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_mtp_F: B_RadioBag_01_mtp_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_tropic_F: B_RadioBag_01_tropic_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_oucamo_F: B_RadioBag_01_oucamo_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+	class STAF_B_RadioBag_01_wdl_F: B_RadioBag_01_wdl_F {
+    scope = 2;
+    scopeCurator = 2;
+    maximumLoad = 160;
+    mass = 160;
+
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+	};
+};
