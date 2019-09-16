9a7698e3505c649bd79ad8a29bcef1ddb35eeb45
Moony <wildsevenmusic@googlemail.com>
2019-09-16 14:27:21 +0200

    Added Mod to add Snow to Vidda

---
+++ Source/@STAF_Misc/addons/staf_vidda_snow/CfgPatches.hpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class STAF_Vidda_Snow
+	{
+		units[] =	{};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"blud_vidde"};
+		author = "STAF";
+		authorUrl = "http://stafclan.com/index.php/BoardList/";
+		version = 1.0;
+		versionStr = "1.0";
+		versionAr[] = {1,0};
+	};
+};
